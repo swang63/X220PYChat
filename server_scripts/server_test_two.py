7367cfa7f94b511c34c9a73958cdc883f4007630
dprefontaine <43427173+dprefontaine@users.noreply.github.com>
2022-03-30 00:28:24 -0400

    usernames and commands
    chat now stores usernames associated with a user
    
    server now sends back certain data when using a slash first

--- server_scripts/server_test_two.py
+++ server_scripts/server_test_two.py
@@ -26,17 +26,38 @@ def read(connection, mask):
 	
 	if sentence:
 		sentence = sentence.decode()
+		split_sentence = sentence.split(": ", 1)
 		
-		# GET THE IP's CHAT NUMBER
-		chat_number = chat.get_user(host)
+		#FOR COMMANDS
+		if (split_sentence[1][0] == '/'):
+			command = split_sentence[1][1:]
+			
+			#SWITCH OF COMMANDS
+			commands = {
+				"get_users": chat.get_usernames(),
+				"get_messages": chat.return_messages()
+			}
+			
+			command_result = commands.get(command, "No command selected")
+			
+			connection.send(command_result.encode())
 		
-		# SEND TO CHAT OBJECT
-		sentence = "[" + str(chat_number) + "] " + sentence
-		chat.insert_message(sentence)
-		allSentences = chat.return_messages()
-		   
-		connection.send(allSentences.encode())
-		print(allSentences)
+		#FOR MESSAGES
+		else:
+			# GET THE IP's CHAT NUMBER
+			chat_number = chat.get_user(host)
+			
+			chat.set_username(chat_number, split_sentence[0])
+			
+			# SEND TO CHAT OBJECT
+			sentence = "[" + str(chat_number) + "] " + sentence
+			chat.insert_message(sentence)
+			allSentences = chat.return_messages()
+			   
+			confirm_chat = "Message accepted"
+			
+			connection.send(confirm_chat.encode())
+			print(allSentences)
 	else:
 		sel.unregister(connection)
 		connection.close()
